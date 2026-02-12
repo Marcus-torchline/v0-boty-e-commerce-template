9dce43d32caf7cb00d5ba9f998d9144c1b02002e
florian-torchline <florian@torchlinegroup.com>
2026-02-12 18:00:36 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -13,7 +13,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     },
     { url: 'https://confitone.com/#our-story',
      changeFrequency: 'weekly',
-     priority: 0.9
+     priority: 0.7
     },
   ]
 }
