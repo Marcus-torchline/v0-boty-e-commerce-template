6f5d149d88af36fe639a3fccbf767b3ad548fe65
florian-torchline <florian@torchlinegroup.com>
2026-02-12 17:49:25 -0500

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://confitone.com',
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    { url: 'https://confitone.com/#our-products',
+     changeFrequency: 'weekly',
+     priority: 0.9
+    },
+    { url: 'https://confitone.com/#our-story',
+     changeFrequency: 'weekly',
+     priority: 0.9
+    },
+  ]
+}
