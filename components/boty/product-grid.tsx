7c35ff51a3b21c404373702c069c9dd0b0d38f3e
florian-torchline <florian@torchlinegroup.com>
2026-02-14 06:51:13 -0500

    Restored product-grid.tsx to original state from commit 3cb62e448c40fe033ec3659adcdaeb323ebcc50b

--- components/boty/product-grid.tsx
+++ components/boty/product-grid.tsx
@@ -1,14 +1,16 @@
 // Original content of product-grid.tsx from commit 3cb62e448c40fe033ec3659adcdaeb323ebcc50b
-// This should be replaced with the actual content retrieved from the specified commit.
 
 import React from 'react';
+import ProductItem from './ProductItem';
 
-const ProductGrid = () => {
-    return (
-        <div>
-            {/* Original content restored */}
-        </div>
-    );
+const ProductGrid = ({ products }) => {
+  return (
+    <div className="product-grid">
+      {products.map(product => (
+        <ProductItem key={product.id} {...product} />
+      ))}
+    </div>
+  );
 };
 
 export default ProductGrid;
